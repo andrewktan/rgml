b874a5647f47338948c31e735c717c96d610fd47
Andrew Tan <andrew@nat-tvwna-inside-visitornet101-a-8931.princeton.org>
2018-06-11 23:32:11 -0400

    forgot to lint this file -- fixed

--- ising_data/sample_gen.py
+++ ising_data/sample_gen.py
@@ -1,5 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 # parameters
 numsamples = 20000
@@ -8,7 +8,7 @@ numsamples = 20000
 sz = 25
 beta = 0.50  # inv temperature
 J = 1  # nearest neighbor coupling
-h = 0 #0.1  # external field
+h = 0  # 0.1  # external field
 
 Z = sz*sz*50  # iterations
 
@@ -16,7 +16,7 @@ Z = sz*sz*50  # iterations
 
 for ns in range(numsamples):
     # initialize random field
-#    beta = 1 / (np.random.rand() * 300)
+    #    beta = 1 / (np.random.rand() * 300)
 
     field = np.random.randint(2, size=(sz, sz)) * 2 - 1
 
@@ -25,28 +25,27 @@ for ns in range(numsamples):
 
         # calculate energy difference
         s = 0
-        s += field[np.mod(r+1,sz), np.mod(c,sz)]
-        s += field[np.mod(r-1,sz), np.mod(c,sz)]
-        s += field[np.mod(r,sz), np.mod(c+1,sz)]
-        s += field[np.mod(r,sz), np.mod(c-1,sz)]
-        s *= field[r,c]
-        
-        pflip = 1 / np.exp(2*beta*(J*s + h*field[r,c]))
-            
+        s += field[np.mod(r+1, sz), np.mod(c, sz)]
+        s += field[np.mod(r-1, sz), np.mod(c, sz)]
+        s += field[np.mod(r, sz), np.mod(c+1, sz)]
+        s += field[np.mod(r, sz), np.mod(c-1, sz)]
+        s *= field[r, c]
+
+        pflip = 1 / np.exp(2*beta*(J*s + h*field[r, c]))
+
         if np.random.rand() < pflip:
-            field[r,c] *= -1
+            field[r, c] *= -1
 
 #    plt.matshow(field, cmap=plt.cm.gray)
 #    plt.show()
 
-    sample = field.reshape((1,-1))
+    sample = field.reshape((1, -1))
     label = np.array([beta])
 
-    with open('data_0_50.txt','ab') as datafile:
+    with open('data_0_50.txt', 'ab') as datafile:
         np.savetxt(datafile, sample.astype(int), fmt='%i')
 
 #    with open('labels.txt','ab') as labelfile:
 #        np.savetxt(labelfile, label)
 
-    print(ns, '%.4f' % beta) 
-
+    print(ns, '%.4f' % beta)
