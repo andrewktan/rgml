e5f7236d557fe524db9b12feb94844372014c557
Andrew Tan <andrewtsoc@gmail.com>
2018-07-18 20:36:53 -0400

    New DIB clustering method.

---
+++ scripts/information_bottleneck_q.py
@@ -0,0 +1,229 @@
+from itertools import combinations
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class DIB:
+    eps = 1e-12
+
+    def __init__(self, pxx, beta=5, hiddens=100, iterations=10):
+        self.beta = beta
+        self.hiddens = hiddens
+        self.iterations = iterations    # random initializations
+
+        self.pxx = pxx  # joint distribution, 2D numpy array
+        self.xsz = pxx.shape[0]
+
+        # calculate marginals
+        self.px = pxy.sum(axis=1)
+        self.px = divide(self.px, self.px.sum())
+
+        self.beta = beta
+
+        np.random.seed(0)
+
+    def compress(self, epsilon=1e-4):
+        self._initialize_clusters()
+        self._update()
+
+        prev_cost = 1e6
+
+        idx = 0
+
+        while abs(self.cost - prev_cost) > epsilon:
+            print("Iteration %d: %.2f" % (idx, self.cost))
+            prev_cost = self.cost
+            self._step()
+            self._cleanup()
+            self._update()
+            idx += 1
+
+        self._cleanup()
+
+        return self.cost
+
+# core DIB helper functions #
+    def _initialize_clusters(self):
+        """
+        randomly intialize clusters
+        """
+        # initialize clusters
+        x = np.eye(self.hiddens)
+        qt_x = x[:, np.random.randint(self.hiddens, size=self.xsz)]
+        self.f = np.argmax(qt_x, axis=0)
+        self.l = np.zeros((self.xsz, self.hiddens))
+
+        self.cost = 0     # effectively infinity
+
+    def _step(self):
+        """
+        updates cluster assignments by maximizing DIB objective
+        """
+        qt_x = self.l.T     # not correct, but should work for DIB scheme
+
+        f = np.argmax(qt_x, axis=0)
+
+        self.f = f
+
+    def _update(self):
+        """
+        recalculates q(t) and q(t|x) for current cluster assignments
+        """
+        self.qt = np.zeros(self.hiddens)
+        self.qy_t = np.zeros((self.hiddens, self.hiddens))
+        self.qy_x = np.zeros((self.hiddens, self.xsz))
+
+        for x in range(self.xsz):
+            t = self.f[x]
+            self.qt[t] += self.px[x]
+
+        for x in range(self.xsz):
+            for xp in range(self.xsz):
+                t = self.f[x]
+                y = self.f[xp]
+                self.qy_t[y, t] += divide(self.pxx[x, xp], self.qt[t])
+                self.qy_x[y, x] += divide(self.pxx[x, xp], self.px[x])
+
+        d = np.zeros((self.xsz, self.hiddens))
+        for x in range(self.xsz):   # can this be simplified?
+            for t in range(self.hiddens):
+                for y in range(self.ysz):
+                    d[x, t] += self.qy_x[y, x] * (
+                        np.log2(self.qy_x[y, x] + DIB.eps) -
+                        np.log2(self.qy_t[y, t] + DIB.eps))
+
+        self.l = np.log2(self.qt + DIB.eps) - self.beta * d
+
+        self.cost = self._calculate_cost(self.qy_t, self.qt)
+
+    def _calculate_cost(self, qy_t, qt):
+        """
+        calculate the DIB cost function of a proposed clustering
+        """
+        cost = 0
+        for t in range(self.hiddens):
+            cost -= qt[t] * np.log2(qt[t] + DIB.eps)
+
+        for y in range(self.ysz):
+            for t in range(self.hiddens):
+                cost -= (qy_t[y, t] * qt[t]) * (
+                    np.log2(qy_t[y, t] + DIB.eps) -
+                    np.log2(self.py[y] + DIB.eps))
+
+        return cost
+
+    def _cleanup(self):
+        """
+        cleanup and relabel unused clusters
+        """
+        # relabel clusters
+        uniques = np.unique(self.f)
+        unique_map = {v: i for i, v in enumerate(uniques)}
+        self.f = np.vectorize(unique_map.get)(self.f)
+
+        # remove unused bins
+        self.qy_t = self.qy_t[:, np.sum(self.qy_t, axis=0) != 0]
+        self.qt = self.qt[self.qt != 0]
+
+        self.hiddens = uniques.size
+
+# reporting and visualization #
+
+    def report_clusters(self):
+        """
+        returns current cluster assignmetns
+        """
+        print("Found %d clusters with beta = %.2f" %
+              (np.unique(self.f).size, self.beta))
+        return self.f
+
+    def visualize_clusters(self, vsz=3, debug=False):
+        """
+        displays clusters
+        """
+        qx_t = np.zeros((vsz*vsz, self.hiddens))
+        finv = {x: set() for x in range(self.hiddens)}
+
+        self.finv = finv
+
+        for idx, element in enumerate(self.f):
+            finv[element].add(idx)
+
+        print({t: np.mean([bin(x).count('1') for x in finv[t]]) for t in finv})
+        for t in finv:
+            qx_t[:, t] = np.sum(
+                np.array([self._bin2row(x, vsz*vsz) * self.px[x]
+                          for x in finv[t]]), axis=0)
+
+        # normalize cluster averages
+        qx_t = qx_t / self.qt
+
+        # reorder clusters by probability
+        order = np.argsort(-self.qt)
+        qx_t = qx_t[:, order]
+
+        # reshape and plot
+        if debug:
+            clusters = np.zeros((vsz, vsz*self.hiddens))
+
+            for t in finv:
+                clusters[:, t*vsz:(t+1)*vsz] = \
+                    qx_t[:, t].reshape(vsz, vsz)
+
+            plt.matshow(clusters, cmap=plt.cm.gray)
+            plt.show()
+
+        return qx_t
+
+    def mi_relevant(self):
+        """
+        returns mutual information between cluster assignments and environment
+        I(T;Y)
+        """
+        mi = 0
+
+        for y in range(self.ysz):
+            for t in range(self.hiddens):
+                mi += self.qy_t[y, t] * self.qt[t] * (
+                    np.log2(self.qy_t[y, t] + DIB.eps) -
+                    np.log2(self.py[y] + DIB.eps))
+
+        return mi
+
+    def mi_captured(self):
+        """
+        returns mutual information between cluster assignments and environment
+        I(T;X)
+        """
+        mi = 0
+
+        for x in range(self.xsz):
+            for t in range(self.hiddens):
+                mi += (self.f[x] == t) * self.px[x] * (
+                    np.log2((self.f[x] == t) + DIB.eps) -
+                    np.log2(self.qt[t] + DIB.eps))
+
+        return mi
+
+    def _bin2row(self, x, sz=9):
+        """
+        converts x to binary row (big endian)
+        - this could probably be more efficient
+        """
+        return np.array([(x//2**k) % 2 == 1 for k in range(sz)])
+
+
+# helpful functions #
+
+def debugshow(thing):
+    plt.matshow(thing, cmap=plt.cm.gray)
+    plt.show()
+
+
+def divide(a, b):
+    """
+    a / b, b==0 not used
+    a better divide function
+    """
+    return np.divide(a, b, out=np.zeros_like(a), where=b != 0)
---
+++ scripts/isingib_q.py
@@ -0,0 +1,115 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from information_bottleneck import *
+from ising_iterator import *
+
+# parameters #
+##############
+
+perform_beta_sweep = True
+perform_demo = False
+
+symmetrize = True
+
+dfile = '/Users/andrew/Documents/rgml/ising_data/data_0_45'
+savefile = 'ising_ib_joint.npy'
+sz = 81     # sz of the samples (sq)
+vsz = 3   # sz of visible block (sq)
+stride = 3
+tsz = 1000000   # table sz
+
+# load data #
+#############
+
+
+def to_code(spins):
+    ret = 0
+    for i, j in enumerate(spins):
+        j = 1 if j == 1 else 0
+        ret += j << i
+
+    return ret
+
+
+def calculate_joint(eloc=4):
+    """
+    Calculates the joint p(x,x')
+    """
+    thist = np.zeros((2**(vsz**2), 2**(vsz**2)))
+
+    for _, sample in zip(range(tsz), IsingIterator(dfile, img_size=sz)):
+        vcode = to_code(np.reshape(sample[0:vsz, 0:vsz], -1))
+        ecode = to_code(np.reshape(sample[eloc:eloc+vsz, eloc:eloc+vsz], -1))
+
+        thist[vcode, ecode] += 1
+
+    thist /= tsz
+
+    return thist
+
+
+# calculate and store if necessary #
+####################################
+
+try:
+    joint_file = open(savefile, 'rb')
+except IOError:
+    print("Computing new joint distribution")
+    thist = calculate_joint()
+    joint_file = open(savefile, 'wb')
+    np.save(joint_file, thist)
+else:
+    print("Loading saved joint distribution")
+    thist = np.load(joint_file)
+
+# information bottleneck test #
+###############################
+if symmetrize:
+    thist = (thist + thist.T)/2
+
+if perform_demo:
+    dib = DIB(thist, beta=5, hiddens=50)
+    dib.compress()
+    dib.report_clusters()
+    c = dib.visualize_clusters(debug=True)
+
+# beta sweep #
+##############
+
+if perform_beta_sweep:
+    betas = np.arange(0, 30.1, 0.5)
+    info_y = np.zeros_like(betas, dtype=np.float32)
+    info_x = np.zeros_like(betas, dtype=np.float32)
+    clusters = {x: [] for x in range(1, 100)}
+
+    for i, beta in enumerate(betas):
+        dib = DIB(thist, beta=beta, hiddens=100)
+        dib.compress()
+        f = dib.report_clusters()
+        info_y[i] = dib.mi_relevant()
+        info_x[i] = dib.mi_captured()
+        clusters[np.unique(f).size].append(beta)
+
+    # calculate kink angles
+    angles = {k: np.pi/2 - np.arctan(np.min(v + [1e3])) -
+              np.arctan(1/np.max(v + [1e-3]))
+              for k, v in clusters.items()}
+    angles = {k: v * 180/np.pi for k, v in angles.items()}
+    angles = {k: max(v, 0) for k, v in angles.items()}
+
+    plt.plot(info_x, info_y, 'ko')
+    plt.title('DIB Plane Plot')
+    plt.xlabel('H(T)')
+    plt.ylabel('I(Y;T)')
+    plt.show()
+
+    with open("ipdata_%02d.pkl" % (2*bsz+1), 'wb') as f:
+        dump = {}
+        dump['info_x'] = info_x
+        dump['info_y'] = info_y
+        dump['angles'] = angles
+        dump['betas'] = betas
+        dump['clusters'] = clusters
+        dump['theta'] = np.array([angles[k] for k in clusters])
+        pickle.dump(dump, f)
