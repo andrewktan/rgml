ffdec040969b3f59573639bddd583e3038fa8e3b
Andrew Tan <andrewtsoc@gmail.com>
2018-07-30 14:16:08 -0400

    Just saving...

--- cvib2/vae.py
+++ cvib2/vae.py
@@ -14,10 +14,10 @@ class VariationalAutoencoder():
                  dstrain,
                  dstest,
                  latent_dimensions=5,
-                 num_epochs=30,
-                 learning_rate=2.5e-5,
-                 num_train=32,
-                 batch_size=32):
+                 num_epochs=500,
+                 learning_rate=1e-4,
+                 num_train=1,
+                 batch_size=1):
 
         # hyperparameters
         self.LATENT_DIM = latent_dimensions
@@ -66,6 +66,27 @@ class VariationalAutoencoder():
         self._decoder_network()
 
     def _create_loss(self):
+        with tf.name_scope('loss_layer'):
+            # the vectorized input
+            x_vectorized = self.input_image_raw
+            x_reconstr_mean_vectorized = tf.reshape(
+                self.x_reconstr_mean, [-1, 1024], name='x_reconstr_mean_vectorized')
+
+            pixel_loss = tf.reduce_sum(
+                tf.square(x_reconstr_mean_vectorized - x_vectorized), 1)
+            self.pixel_loss = pixel_loss
+
+            self.latent_loss = -0.5 * \
+                tf.reduce_sum(1 + self.z_log_sigma_sq -
+                              tf.square(self.z_mean) - tf.exp(self.z_log_sigma_sq), 1)
+
+            self.latent_loss_mean = tf.reduce_mean(self.latent_loss)
+            self.pixel_loss_mean = tf.reduce_mean(self.pixel_loss)
+
+            self.cost = tf.reduce_mean(
+                self.latent_loss + self.pixel_loss, name='cost_function')   # average over batch
+
+    def _create_loss2(self):
         with tf.variable_scope('loss_layer'):
             x_vectorized = self.input_image_raw
 
@@ -75,7 +96,7 @@ class VariationalAutoencoder():
             x_reconstr_logits_vectorized = tf.reshape(
                 self.x_reconstr_logits, [-1, 1024], name='x_reconstr_logits_vectorized')
 
-            pixel_loss = tf.reduce_mean(
+            pixel_loss = tf.reduce_sum(
                 tf.square(x_reconstr_mean_vectorized - x_vectorized), 1)
             # pixel_loss = tf.reduce_mean(
             # tf.nn.sigmoid_cross_entropy_with_logits(
@@ -96,7 +117,7 @@ class VariationalAutoencoder():
             self.train_step = tf.train.AdamOptimizer(
                 learning_rate=self.lr_placeholder).minimize(self.cost, var_list=variables)
 
-    def _encoder_network(self):
+    def _encoder_network2(self):
         # conv 1.1
         with tf.variable_scope('enc_conv1_1'):
             kernel = tf.get_variable(name='weights',
@@ -245,7 +266,171 @@ class VariationalAutoencoder():
 
             self.parameters += [fc_w, fc_b]
 
+    def _encoder_network(self):
+        images = self.input_image
+
+        # conv1_
+        with tf.name_scope('enc_conv1_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal(
+                [3, 3, 1, 32], dtype=tf.float32, stddev=2.0/np.sqrt(27 + 64)), name='weights', trainable=True)
+            conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(
+                0.1, shape=[32], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.enc_conv1_1 = tf.nn.relu(out, name=scope)
+            self.parameters += [kernel, biases]
+
+        # conv1_2
+        with tf.name_scope('enc_conv1_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal(
+                [3, 3, 32, 32], dtype=tf.float32, stddev=2.0/np.sqrt(144 + 64)), name='weights')
+            conv = tf.nn.conv2d(self.enc_conv1_1, kernel, [
+                                1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(
+                0.1, shape=[32], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.enc_conv1_2 = tf.nn.relu(out, name=scope)
+            self.parameters += [kernel, biases]
+
+        # pool1
+        with tf.name_scope('enc_pooling1'):
+            self.pool1 = tf.nn.max_pool(self.enc_conv1_2,
+                                        ksize=[1, 2, 2, 1],
+                                        strides=[1, 2, 2, 1],
+                                        padding='SAME',
+                                        name='enc_pool1')
+
+        # conv2_1
+        with tf.name_scope('enc_conv2_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal(
+                [3, 3, 32, 64], dtype=tf.float32, stddev=2.0/np.sqrt(144 + 16)), name='weights')
+            conv = tf.nn.conv2d(self.pool1, kernel, [
+                                1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(
+                0.1, shape=[64], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.enc_conv2_1 = tf.nn.relu(out, name=scope)
+            self.parameters += [kernel, biases]
+
+        # conv2_2
+        with tf.name_scope('enc_conv2_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal(
+                [3, 3, 64, 64], dtype=tf.float32, stddev=2.0/np.sqrt(144+16)), name='weights')
+            conv = tf.nn.conv2d(self.enc_conv2_1, kernel, [
+                                1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(
+                0.1, shape=[64], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.enc_conv2_2 = tf.nn.relu(out, name=scope)
+            self.parameters += [kernel, biases]
+
+        # pool2
+        with tf.name_scope('enc_pooling2'):
+            self.pool2 = tf.nn.max_pool(self.enc_conv2_2,
+                                        ksize=[1, 2, 2, 1],
+                                        strides=[1, 2, 2, 1],
+                                        padding='SAME',
+                                        name='enc_pool2')
+
+        prev_dim = int(np.prod(self.pool2.get_shape()[1:]))
+
+        pool2_flat = tf.reshape(self.pool2, [-1, prev_dim])
+        # fc1
+        with tf.name_scope('enc_z_mean') as scope:
+            # the total number of features extracted per input image
+            fc_w = tf.Variable(tf.truncated_normal(
+                [prev_dim, self.LATENT_DIM], dtype=tf.float32, stddev=1.0/np.sqrt(prev_dim + self.LATENT_DIM)), name='weights')
+            fc_b = tf.Variable(tf.constant(
+                0.1, shape=[self.LATENT_DIM], dtype=tf.float32), trainable=True, name='biases')
+            self.z_mean = tf.nn.bias_add(tf.matmul(pool2_flat, fc_w), fc_b)
+            self.parameters += [fc_w, fc_b]
+
+        with tf.name_scope('enc_z_variance') as scope:
+            # the total number of features extracted per input image
+            fc_w = tf.Variable(tf.truncated_normal(
+                [prev_dim, self.LATENT_DIM], dtype=tf.float32, stddev=2.0/np.sqrt(prev_dim + self.LATENT_DIM)), name='weights')
+            fc_b = tf.Variable(tf.constant(
+                0.1, shape=[self.LATENT_DIM], dtype=tf.float32), trainable=True, name='biases')
+            self.z_log_sigma_sq = tf.nn.bias_add(
+                tf.matmul(pool2_flat, fc_w), fc_b)
+            self.parameters += [fc_w, fc_b]
+
     def _decoder_network(self):
+        # the size that ends up at the encoder is 8 x 8 x 64
+        with tf.name_scope('dec_fc'):
+            fc_g_w = tf.Variable(tf.truncated_normal(
+                [self.LATENT_DIM, 4096], dtype=tf.float32, stddev=1.0/np.sqrt(self.LATENT_DIM + 2048)), name='weights')
+            fc_g_b = tf.Variable(tf.constant(
+                0.1, shape=[4096], dtype=tf.float32), trainable=True, name='biases')
+            a_1 = tf.nn.bias_add(tf.matmul(self.z, fc_g_w), fc_g_b)
+            self.g_1 = tf.nn.relu(a_1)
+            self.parameters += [fc_g_w, fc_g_b]
+
+        with tf.name_scope('dec_reshape'):
+            g_1_images = tf.reshape(self.g_1, [-1, 8, 8, 64])
+
+        # scale up to size 16 x 16 x 64
+        resized_1 = None
+        with tf.name_scope('dec_resize1'):
+            resized_1 = tf.image.resize_images(
+                g_1_images, [16, 16], method=tf.image.ResizeMethod.BILINEAR)
+
+        # conv1_1
+        with tf.name_scope('dec_conv1_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal(
+                [3, 3, 64, 32], dtype=tf.float32, stddev=2.0/np.sqrt(288 + 16)), name='weights')
+            conv = tf.nn.conv2d(resized_1, kernel, [
+                                1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(
+                0.0, shape=[32], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.dec_conv1_1 = tf.nn.relu(out, name=scope)
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('dec_conv1_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal(
+                [3, 3, 32, 32], dtype=tf.float32, stddev=2.0/np.sqrt(144 + 16)), name='weights')
+            conv = tf.nn.conv2d(self.dec_conv1_1, kernel, [
+                                1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(
+                0.0, shape=[32], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.dec_conv1_2 = tf.nn.relu(out, name=scope)
+            self.parameters += [kernel, biases]
+
+        # scale up to size 32 x 32 x 32
+        resized_2 = None
+        with tf.name_scope('dec_resize2'):
+            resized_2 = tf.image.resize_images(
+                self.dec_conv1_2, [32, 32], method=tf.image.ResizeMethod.BILINEAR)
+
+        with tf.name_scope('dec_conv2_1') as scope:
+            kernel = tf.Variable(tf.truncated_normal(
+                [3, 3, 32, 3], dtype=tf.float32, stddev=2.0/np.sqrt(144 + 3)), name='weights')
+            conv = tf.nn.conv2d(resized_2, kernel, [
+                                1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(
+                0.0, shape=[3], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.dec_conv2_1 = tf.nn.relu(out, name=scope)
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('dec_conv2_2') as scope:
+            kernel = tf.Variable(tf.truncated_normal(
+                [3, 3, 3, 1], dtype=tf.float32, stddev=2.0/np.sqrt(27 + 3)), name='weights')
+            conv = tf.nn.conv2d(self.dec_conv2_1, kernel, [
+                                1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(
+                0.0, shape=[1], dtype=tf.float32), trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.dec_conv2_2 = out
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('dec_output'):
+            self.x_reconstr_logits = self.dec_conv2_2
+            self.x_reconstr_mean = tf.sigmoid(self.dec_conv2_2)
+
+    def _decoder_network2(self):
         with tf.variable_scope('dec_fc'):
             fc_g_w = tf.get_variable(name='weights',
                                      shape=[self.LATENT_DIM, 4096],
@@ -287,7 +472,7 @@ class VariationalAutoencoder():
                                 padding='SAME')
 
             biases = tf.Variable(
-                tf.constant(1000.0, shape=[32], dtype=tf.float32),
+                tf.constant(0.0, shape=[32], dtype=tf.float32),
                 trainable=True,
                 name='biases')
 
@@ -376,6 +561,95 @@ class VariationalAutoencoder():
             self.x_reconstr_mean = tf.sigmoid(self.x_reconstr_logits)
 
     def train(self, num_epochs_to_display=1):
+        tc = 0
+        fc = 0
+        lc = 0
+        pc = 0
+
+        costs = {}
+        costs['latent'] = []
+        costs['pixel'] = []
+        costs['total'] = []
+
+        current_lr = 2e-4
+
+        init = tf.initialize_all_variables()
+        saver = tf.train.Saver(self.parameters)
+
+        current_epoch_cost = 0
+        current_rec_cost = 0
+        current_lat_cost = 0
+        current_pix_cost = 0
+
+        ITERATIONS_PER_EPOCH = int(self.NUM_TRAIN/self.BATCH_SIZE)
+
+        with tf.Session() as sess:
+            sess.run(init)
+
+            batch_images, _ = self.dstrain.next_batch()
+
+            t0 = time.time()
+
+            eps = np.random.normal(loc=0.0, scale=1.0, size=(
+                self.BATCH_SIZE, self.LATENT_DIM))
+            tc, lc, pc = sess.run([self.cost, self.latent_loss_mean, self.pixel_loss_mean], feed_dict={
+                self.input_image_raw: batch_images, self.eps_placeholder: eps})
+
+            # Append them to lists
+            costs['total'].append(fc)
+            costs['pixel'].append(pc)
+            costs['latent'].append(lc)
+
+            t1 = time.time()
+            print(
+                'Initial Cost: {:2f}  = {:.2f} L + {:.2f} P -- time taken {:.2f}'.format(tc, lc, pc, t1-t0))
+
+            t0 = t1
+
+            # Train for several epochs
+            for epoch in range(self.NUM_EPOCHS):
+
+                print("learning rate : {}".format(current_lr))
+
+                for i in range(ITERATIONS_PER_EPOCH):
+                    # pick a mini batch
+                    batch_images, _ = self.dstrain.next_batch()
+                    eps = np.random.normal(loc=0.0, scale=1.0, size=(
+                        self.BATCH_SIZE, self.LATENT_DIM))
+
+                    _, tc, lc, pc = sess.run([self.train_step, self.cost, self.latent_loss_mean, self.pixel_loss_mean], feed_dict={
+                        self.input_image_raw: batch_images, self.eps_placeholder: eps, self.lr_placeholder: current_lr})
+
+                    current_epoch_cost += tc
+                    current_lat_cost += lc
+                    current_pix_cost += pc
+
+                # for displaying costs etc -------------------------------------
+                current_epoch_cost /= ITERATIONS_PER_EPOCH  # average it over the iterations
+                current_lat_cost /= ITERATIONS_PER_EPOCH
+                current_pix_cost /= ITERATIONS_PER_EPOCH
+                costs['total'].append(current_epoch_cost)
+                costs['latent'].append(current_lat_cost)
+                costs['pixel'].append(current_pix_cost)
+                # --------------------------------------------------------------
+
+                # print stats --------------------------------------------------
+                if epoch % num_epochs_to_display == 0:
+                    t1 = time.time()
+                    print(' epoch: {}/{} -- cost {:.2f} = {:.2f} L + {:.2f} P -- time taken {:.2f}'.format(
+                        epoch+1, self.NUM_EPOCHS, current_epoch_cost, current_lat_cost, current_pix_cost, t1-t0))
+                    # Reset the timer
+                    t0 = t1
+                # --------------------------------------------------------------
+
+                # Reset the costs for next epoch -------------------------------
+                current_epoch_cost = 0
+                current_rec_cost = 0
+                current_lat_cost = 0
+                current_pix_cost = 0
+                # --------------------------------------------------------------
+
+    def train2(self, num_epochs_to_display=1):
         tc = 0
         fc = 0
         lc = 0
