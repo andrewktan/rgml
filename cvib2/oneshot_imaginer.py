2cf15b1ed3cdaebe372a371acc26f3a0edf0690d
Andrew Tan <andrewtsoc@gmail.com>
2018-08-13 16:26:33 -0400

    Oops. fixed.

--- cvib2/oneshot_imaginer.py
+++ cvib2/oneshot_imaginer.py
@@ -14,12 +14,12 @@ from vae_utils import *
 if __name__ == '__main__':
     # import dataset
     if args.dataset == 'cifar10':
-    (image_train, label_train), (image_test, label_test) = cifar10.load_data()
+        (image_train, label_train), (image_test, label_test) = cifar10.load_data()
 
-    image_train = np.reshape(image_train, [-1, 32, 32, 3])
-    image_test = np.reshape(image_test, [-1, 32, 32, 3])
-    image_train = image_train.astype('float32') / 255
-    image_test = image_test.astype('float32') / 255
+        image_train = np.reshape(image_train, [-1, 32, 32, 3])
+        image_test = np.reshape(image_test, [-1, 32, 32, 3])
+        image_train = image_train.astype('float32') / 255
+        image_test = image_test.astype('float32') / 255
 
     elif args.dataset == 'ising':
         with open('/Users/andrew/Documents/rgml/ising_data/data_0_45.pkl', 'rb') as f:
